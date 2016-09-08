238d5e9b9b08cda6cde4c2597d843c50b4262f2c
zhuang <2384108741@qq.com>
2016-09-08 08:25:18 +0800

    front login

---
+++ app/Http/Controllers/Front/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+/*********************************************************************************
+ *  后台登陆控制器
+ *-------------------------------------------------------------------------------
+ * 版权所有: CopyRight By
+ * 文件内容简单说明
+ *-------------------------------------------------------------------------------
+ * $FILE:BaseModel.php
+ * $Author:pzz
+ * $Dtime:2016/9/7
+ ***********************************************************************************/
+
+namespace App\Http\Front;
+
+
+use App\Http\Controllers\FrontController;
+use Illuminate\Http\Request;
+
+class LoginController extends FrontController
+{
+    public function __construct()
+    {
+    }
+
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
+
+        }
+
+        return view('front.login');
+    }
+
+    public function register(Request $request)
+    {
+        if ($request->isMethod('post')) {
+
+        }
+
+        return view('front.register');
+    }
+
+}
--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -1,18 +1,21 @@
 <?php
 /*********************************************************************************
- *  系统前台 - 路由
+ * 前台基础控制器
  *-------------------------------------------------------------------------------
  * 版权所有: CopyRight By
  * 文件内容简单说明
  *-------------------------------------------------------------------------------
- * $FILE:BaseModel.php
+ * $FILE:AdminController.php
  * $Author:pzz
  * $Dtime:2016/9/7
  ***********************************************************************************/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+namespace App\Http\Controllers;
 
-Route::match(['get', 'post'], 'user/login.html', ['as' => 'front.login', 'uses' => 'LoginController@login']);
-Route::match(['get', 'post'], 'user/register.html', ['as' => 'front.register', 'uses' => 'LoginController@register']);
+
+class FrontController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
