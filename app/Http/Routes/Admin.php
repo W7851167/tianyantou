b8c1b1a853621e9a324c5a74788804d34376cf41
zhuxishun <zhuxishun@cw100.com>
2016-09-08 14:42:16 +0800

    11111

--- app/Http/Routes/Admin.php
+++ app/Http/Routes/Admin.php
@@ -14,6 +14,15 @@ Route::group(['as' => 'admin.'], function () {
     Route::match(['get', 'post'], 'login', ['as' => 'login', 'uses' => 'LoginController@login']);
     Route::get('dashboard', ['as'=>'home.index','uses'=>'HomeController@index']);
     Route::get('user', ['as'=>'user.index','uses'=>'UserController@index']);
+    Route::get('user/create', ['as'=>'user.create','uses'=>'UserController@create']);
+    Route::get('user/store/{id}', ['as'=>'user.store','uses'=>'UserController@store']);
+    Route::get('task', ['as'=>'task.index','uses'=>'TaskController@index']);
+    Route::get('task/create', ['as'=>'task.create','uses'=>'TaskController@create']);
+    Route::get('task/store/{id}', ['as'=>'task.store','uses'=>'TaskController@store']);
+    Route::get('task/corp', ['as'=>'task.corp','uses'=>'TaskController@crop']);
+    Route::get('task/crop-create', ['as'=>'task.corp.create','uses'=>'TaskController@corpCreate']);
+    Route::get('task/crop-store/{id}', ['as'=>'task.corp.store','uses'=>'TaskController@cropStore']);
+    Route::get('census', ['as'=>'census','uses'=>'CensusController@index']);
 });
 
 Route::get('test','TestController@index');
