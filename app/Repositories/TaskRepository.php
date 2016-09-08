84f008bc89acd8354ead2fbdd3e720f3730669fe
zhuxishun <zhuxishun@cw100.com>
2016-09-08 09:40:45 +0800

    11111

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,28 @@
+<?php
+/*********************************************************************************
+ *  PhpStorm - phpad
+ *-------------------------------------------------------------------------------
+ * 版权所有: CopyRight By cw100.com
+ * 文件内容简单说明
+ *-------------------------------------------------------------------------------
+ * $FILE:TaskRepository.php
+ * $Author:zxs
+ * $Dtime:2016/9/8
+ ***********************************************************************************/
+
+namespace App\Repositories;
+
+
+use App\Models\CorpModel;
+use App\Models\TaskModel;
+
+class TaskRepository extends  BaseRepository
+{
+    public function __construct(
+        TaskModel $taskModel,
+        CorpModel $corpModel
+    ) {
+        $this->taskModel = $taskModel;
+        $this->corpModel = $corpModel;
+    }
+}
