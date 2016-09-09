c259e9d9b645792ab4f114e92e125c5dc0a6ae41
zhuxishun <zhuxishun@cw100.com>
2016-09-09 21:34:56 +0800

    1111

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -4,7 +4,7 @@
             <a href="javascript:void(0)"><img src="{!! url('admin/images/user-small.png')!!}"/></a>
             <div class="content-left-header-pic-page">
                 <span>{!! \Session::get('user.login') !!}</span>
-                <p><a href="{!! url('user/password') !!}">修改密码</a></p>
+                <p><a href="{!! url('passport/password') !!}">修改密码</a></p>
                 <p><a href="{!! url('passport/logout') !!}">退出登录</a></p>
             </div>
         </div>
