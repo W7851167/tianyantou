af8f353b44956f8cc6795a487071286aa3f72e2c
zhuxishun <zhuxishun@cw100.com>
2016-09-11 16:50:20 +0800

    领用户模型

---
+++ app/Models/TaskReceiveModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+
+class TaskReceiveModel extends BaseModel
+{
+    public $table = 'task_achieves';
+    protected $primaryKey = 'id';
+
+    /**
+     * 获取任务名称
+     */
+    public function task(){
+        return $this->belongsTo('App\Models\TaskModel','task_id');
+    }
+
+    /**
+     * 获取平台模型
+     */
+    public function corp()
+    {
+        return $this->belongsTo('App\Models\CorpModel','corp_id');
+    }
+
+    /**
+     * 获取领任务用户
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\UserModel','user_id');
+    }
+}
