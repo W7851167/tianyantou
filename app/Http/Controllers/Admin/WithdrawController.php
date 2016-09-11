8ba6dffa87bb38fe6898456a929920b7db31f750
zhuang <2384108741@qq.com>
2016-09-11 16:12:08 +0800

    update nav

--- app/Http/Controllers/Admin/WithdrawController.php
+++ app/Http/Controllers/Admin/WithdrawController.php
@@ -18,20 +18,23 @@ use App\Http\Controllers\AdminController;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 
-class WithdrawController extends  AdminController
+class WithdrawController extends AdminController
 {
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(
+        UserRepository $userRepository
+    )
     {
+        parent::__construct();
         $this->userRepository = $userRepository;
     }
 
     public function index(Request $request)
     {
         $page = !empty($request->get('page')) ? $request->get('page') : 1;
-        list($counts, $lists) = $this->userRepository->getWithdrawList([],$this->perpage, $page);
+        list($counts, $lists) = $this->userRepository->getWithdrawList([], $this->perpage, $page);
         $pageHtml = $this->pager($counts, $page, $this->perpage);
-        return view('admin.withdraw.index',compact('lists','pageHtml'));
+        return view('admin.withdraw.index', compact('lists', 'pageHtml'));
     }
 
     /**
@@ -42,23 +45,23 @@ class WithdrawController extends  AdminController
     public function create(Request $request, $id)
     {
         $withdraw = $this->userRepository->withdrawModel->find($id);
-        if($request->isMethod('post')) {
+        if ($request->isMethod('post')) {
             $data = $request->get('data');
-            if(!($withdraw->user instanceof Model)) {
-                return $this->error('未找到对应账户',null,true);
+            if (!($withdraw->user instanceof Model)) {
+                return $this->error('未找到对应账户', null, true);
             }
-            if(!($withdraw->user->money instanceof Model)) {
-                return $this->error('该账户未有电子钱包',null,true);
+            if (!($withdraw->user->money instanceof Model)) {
+                return $this->error('该账户未有电子钱包', null, true);
             }
-            if($withdraw->user->money->withdraw < $data['price']) {
-                return $this->error('提现冻结金额不足，非法提现',null,true);
+            if ($withdraw->user->money->withdraw < $data['price']) {
+                return $this->error('提现冻结金额不足，非法提现', null, true);
             }
             $result = $this->userRepository->saveWithdraw($data);
-            if($result['status']) {
-                return $this->success('管理员审核用户提现完成', url('withdraw'),true);
+            if ($result['status']) {
+                return $this->success('管理员审核用户提现完成', url('withdraw'), true);
             }
             return $this->error('管理员审核操作异常，请联系开发人员');
         }
-        return view('admin.withdraw.create',compact('withdraw'));
+        return view('admin.withdraw.create', compact('withdraw'));
     }
 }
