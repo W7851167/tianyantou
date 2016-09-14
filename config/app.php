17cd6438780331cceef61193691adfa0e28cb45a
zhuxishun <zhuxishun@cw100.com>
2016-09-14 16:46:20 +0800

    11111

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
 
     'url' => 'http://phpad.net',
     'admin_url' => 'http://admin.phpad.net',
-    'static_url'=>'http://static.phpad.net',
+    'static_url'=>'//static.phpad.net',
     'account_url' =>'http://account.phpad.net',
     /*
     |--------------------------------------------------------------------------
