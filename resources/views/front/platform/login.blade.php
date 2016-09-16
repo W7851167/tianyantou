456452f51423f8bb762b9dcac032911aab381743
zhuxishun <zhuxishun@cw100.com>
2016-09-17 00:24:23 +0800

    111111

--- resources/views/front/platform/login.blade.php
+++ resources/views/front/platform/login.blade.php
@@ -3,7 +3,7 @@
 <p style="width:500px;height:300x;text-align:center; padding:30px;margin:100px auto;border:1px solid #cdcdcd;">
     正在为您跳转至{!! $corp->platform !!}平台...
 </p>
-<form action ="{!! $task->url !!}" method="post">
+<form action ="{!! strpos($task->url,'http') !== false ? $task->url : 'http://' . $task->url !!}" method="post">
     <input type="hidden" name="data" value="{!! \Crypt::encrypt($data)!!}"/>
     <input type="hidden" name="timestamp" value="{!! $sign['timestamp'] !!}"/>
     <input type="hidden" name="nonce" value="{!! $corp->ename !!}"/>
