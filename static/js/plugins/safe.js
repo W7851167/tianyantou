5d51a42148b7c64814fa8b557369249111320895
zhuang <2384108741@qq.com>
2016-09-17 23:20:40 +0800

    safe blade

---
+++ static/js/plugins/safe.js
@@ -0,0 +1,19 @@
+(function() {
+    $('a.inline-modify').click(function(e) {
+        e.preventDefault();
+        var targetSelector = $(this).data('target');
+        var $target = $(targetSelector);
+        if ($target.hasClass('hidden')) {
+            $target.empty();
+            var url = $(this).attr('href');
+            $target.load(url,function(){
+                $target.removeClass('hidden');
+                Ucenter.readaptSidemenuAndMainpanel();
+            });
+
+        } else {
+            $target.empty();
+            $target.addClass('hidden');
+        }
+    });
+})();
