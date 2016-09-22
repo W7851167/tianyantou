5b59afb576d35cfee2ad9a6528a19fa6932a0c16
zhuang <2384108741@qq.com>
2016-09-23 00:32:39 +0800

    update scores

--- resources/views/account/scores/index.blade.php
+++ resources/views/account/scores/index.blade.php
@@ -10,43 +10,39 @@
     <div class="wrap user-center">
         <div class="container clearfix">
             @include('account.common.menu')
-            <!--右边-->
-            <div style="height: 676px;" class="main tworow">
+            <div class="main tworow" style="height: 917px;">
                 <div class="main-inner">
                     <h1 class="section-title">积分明细</h1>
-                    <div class="infobox clearfx">
-                        <div class="integral float_left">
-                            <p>总积分：{!! $total or 0 !!}分</p>
-                        </div>
-                        <div style="clear:both"></div>
-                    </div>
-                    <div class="infobox">
-                        <h1 class="h_short">积分历史记录</h1>
-                        <span id="span_data">
-                            <table cellspacing="0" cellpadding="0" border="0">
-                                <tbody>
-                                <tr>
-                                    <th width="100">时间</th>
-                                    <th width="102">类型</th>
-                                    <th width="108">积分</th>
-                                    {{--<th width="99">剩余积分</th>--}}
-                                    <th width="107">备注</th>
-                                </tr>
+                    <div id="autoreserve_records">
+                        <table class="table table-blue table-bordered ucenter-table" style="font-size:13px;">
+                            <thead>
+                            <tr>
+                                <th>时间</th>
+                                <th>类型</th>
+                                <th>积分</th>
+                                <th>备注</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @if(count($lists) > 0)
                                 @foreach($lists as $sv)
-                                <tr>
-                                    <td width="113" align="center">{!! $sv->created_at or '' !!}</td>
-                                    <td width="102" align="center"> 收入</td>
-                                    <td width="108" align="center">
-                                        <i style="color: red">+ {!! $sv->score or 0 !!}</i>
-                                    </td>
-                                    {{--<td width="91" align="center">{!! $cv->score or 0 !!}</td>--}}
-                                    <td width="99" align="center">{!! $sv->intro or '' !!}</td>
-                                </tr>
+                                    <tr>
+                                        <td>{!! $sv->created_at or '--' !!}</td>
+                                        <td>{!! $sv->score or 0 !!}</td>
+                                        <td>收入</td>
+                                        <td>{!! $sv->intro or '' !!}</td>
+                                    </tr>
                                 @endforeach
-                                </tbody>
-                            </table>
-                            <div class="meneame" style="margin-top: 30px"> {!! $count !!} 条记录 1/1 页</div>
-                        </span>
+                            @else
+                                <tr class="norecord">
+                                    <td colspan="4">
+                                        您还没有任何积分
+                                    </td>
+                                </tr>
+                            @endif
+                            </tbody>
+                        </table>
+                        <div class="pagination" data-pagination-ref="autoreserve_records">{!! $pageHtml !!}</div>
                     </div>
                 </div>
             </div>
