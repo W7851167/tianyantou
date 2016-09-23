dabb536245e5a3c5b83c6ed74612e69ced75c10b
zhuxishun <zhuxishun@cw100.com>
2016-09-24 01:16:54 +0800

    11111

--- resources/views/account/wallet/record.blade.php
+++ resources/views/account/wallet/record.blade.php
@@ -32,18 +32,18 @@
                     <dl class="filter plat-filter">
                         <?php $type = Input::get('opType')?:'all';$time=Input::get('timespantimespan')?:'all'; ?>
                         <dt>操作类型：</dt>
-                        <dd @if($type == 'all')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType=&timespan={!! $time !!}">全部</a></dd>
-                        <dd @if($type == 'invest')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType=invest&timespan={!! $time !!}">投资</a></dd>
-                        <dd @if($type == 'withdraw')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType=withdraw&timespan={!! $time !!}">提现</a></dd>
-                        <dd @if($type == 'other')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType=other&timespan={!! $time !!}">其他</a></dd>
+                        <dd @if($type == 'all')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType=&timespan={!! $time !!}">全部</a></dd>
+                        <dd @if($type == 'invest')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType=invest&timespan={!! $time !!}">投资</a></dd>
+                        <dd @if($type == 'withdraw')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType=withdraw&timespan={!! $time !!}">提现</a></dd>
+                        <dd @if($type == 'other')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType=other&timespan={!! $time !!}">其他</a></dd>
                     </dl>
                     <dl class="filter plat-filter">
                         <dt>查询时间：</dt>
-                        <dd @if($time == 'all')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType={!! $type !!}&timespan=all">全部</a></dd>
-                        <dd @if($time == '1w')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType={!! $type !!}&timespan=1w">一周内</a></dd>
-                        <dd @if($time == '1m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType={!! $type !!}&timespan=1m">一月内</a></dd>
-                        <dd @if($time == '3m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType={!! $type !!}&timespan=3m">三月内</a></dd>
-                        <dd @if($time == '6m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/book.html?opType={!! $type !!}&timespan=6m">半年内</a></dd>
+                        <dd @if($time == 'all')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType={!! $type !!}&timespan=all">全部</a></dd>
+                        <dd @if($time == '1w')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType={!! $type !!}&timespan=1w">一周内</a></dd>
+                        <dd @if($time == '1m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType={!! $type !!}&timespan=1m">一月内</a></dd>
+                        <dd @if($time == '3m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType={!! $type !!}&timespan=3m">三月内</a></dd>
+                        <dd @if($time == '6m')class="active"@endif><a href="{!! config('app.account_url') !!}/wallet/record.html?opType={!! $type !!}&timespan=6m">半年内</a></dd>
                     </dl>
                 </div>
                 <table class="table table-blue table-bordered ucenter-table">
