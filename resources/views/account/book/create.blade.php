33dd15b165525bfbc78db55f8ef5cad0d1af27f4
zhuang <2384108741@qq.com>
2016-09-30 22:05:28 +0800

    blade update

--- resources/views/account/book/create.blade.php
+++ resources/views/account/book/create.blade.php
@@ -160,17 +160,6 @@
                             <input type="submit" class="btn-blue btn-l btn-submit" value="提交">
                         </form>
                     </div>
-                    <div class="tip tab-rules">
-                        <h3 class="title-indent">温馨提示</h3>
-                        <div class="tip-main">
-                            <ul class="tab-content">
-                                <li>1. 为保障您的账户资金安全，绑定银行卡时，您选择的银行卡开户名必须与您实名认证姓名一致；</li>
-                                <li><em>2. 请您提供正确的银行卡信息，如果您提供的银行卡信息有误，2元/笔的可提现手续费银行将不予退还；</em></li>
-                                <li>3. 禁止套现、洗钱行为，绑定银行卡仅限储蓄卡，不支持信用卡绑定提现；</li>
-                                <li>4. 填写银行卡信息时，请确认您的“支行名称”(如上海分行虹口支行)，如您无法确定，请致电您的开户银行客服进行咨询。</li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
