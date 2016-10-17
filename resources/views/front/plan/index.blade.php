589932ad396634dceff1013cd5fa45742f02af4f
zhuang <2384108741@qq.com>
2016-10-17 18:02:26 +0800

    bug

--- resources/views/front/plan/index.blade.php
+++ resources/views/front/plan/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.main')
-@section('title')网贷选平台行情_网贷选平台特色数据_天眼投精选平台搜索@stop
+@section('title')天眼盾用户保障计划_天眼投投资平台@stop
 @section('style')
     <link rel="stylesheet" href="{!! config('app.static_url') !!}/js/lib/laypage//skin/laypage.css !!}" />
     <link rel="stylesheet" href="{!! config('app.static_url') !!}/js/lib/laypage//skin/laypage.css !!}" />
@@ -10,13 +10,9 @@
             <img src="{!! config('app.static_url') !!}/images/plan/tianyandun.jpg">
         </div>
     </div>
-    <!--内容结束-->
 @stop
 @section('script')
     <script type="text/javascript" src="{!!config('app.static_url')!!}/js/lib/laypage/laypage.js?ver={!! time() !!}"></script>
     <script type="text/javascript" src="{!!config('app.static_url')!!}/js/plugins/main.js?ver={!! time() !!}"></script>
     <script type="text/javascript" src="{!!config('app.static_url')!!}/js/lib/laypage/laypage.js?ver={!! time() !!}"></script>
-    <script type="text/javascript" src="{!!config('app.static_url')!!}/js/lib/jquery.form.min.js"></script>
-    <script type="text/javascript" src="{!!config('app.static_url')!!}/js/plugins/form.js?ver={!! time() !!}"></script>
-    <script type="text/javascript" src="{!!config('app.static_url')!!}/js/plugins/login.js?ver={!! time() !!}"></script>
 @stop
