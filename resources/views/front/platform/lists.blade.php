411e40026589011bfeaf1676efa9a5f25919f157
zhuxishun <zhuxishun@cw100.com>
2016-11-01 22:29:16 +0800

    aaa

--- resources/views/front/platform/lists.blade.php
+++ resources/views/front/platform/lists.blade.php
@@ -14,7 +14,7 @@
                 <span class="rate"><em>{!! $cv->min_yield or 0.00 !!}</em>%<em>-</em><em>{!! $cv->max_yield or 0.00 !!}</em>%</span>
                     @endif
             </h4>
-            <span style="width: 300px;margin-left: -15px;">天眼加化率：<em>@if($cv->min_myield == $cv->max_myield){!! $cv->max_myield or 0.00 !!}%@else{!! $cv->min_yield or 0.00 !!}%-{!! $cv->max_yield or 0.00 !!}%@endif</em></span>
+            <span style="width: 300px;margin-left: -15px;">天眼加化率：<em>@if($cv->min_myield == $cv->max_myield){!! $cv->max_myield or 0.00 !!}%@else{!! $cv->min_myield or 0.00 !!}%-{!! $cv->max_myield or 0.00 !!}%@endif</em></span>
             <span>可投标数：<em>{!! $cv->tasks->where('status',1)->count() !!}个</em></span>
             <span>安全评级：<em>{!! $cv->level or 'B' !!}</em></span>
             <a href="platform/{!! $cv->ename !!}.html" target="_blank" class="btn btn-blue-o btn-allwidth">查看详情</a>
